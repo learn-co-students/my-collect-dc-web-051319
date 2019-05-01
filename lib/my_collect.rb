f379172adb5de5280685e5dc7d68c3689d743998
Kolton Starr <starrkolton@gmail.com>
2019-05-01 18:04:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
 
+def my_collect(arr)
+  count = 0 
+  nuarray = []
+  while count < arr.length 
+    x = yield(arr[count])
+    nuarray << x
+  
+    count += 1 
+  end
+  nuarray
+end
+
+#my_collect(list) {|item| item.upcase }
